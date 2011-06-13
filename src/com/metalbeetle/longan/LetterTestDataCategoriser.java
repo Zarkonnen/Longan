e8e98982bdf50781d3f8356d9357cbb6af47f509
David Stark <david.stark@metalbeetle.com>
2011-06-13 12:01:19 +0100

    Added licensing terms.

--- src/com/metalbeetle/longan/LetterTestDataCategoriser.java
+++ src/com/metalbeetle/longan/LetterTestDataCategoriser.java
@@ -1,5 +1,21 @@
 package com.metalbeetle.longan;
 
+/*
+ * Copyright 2011 David Stark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.metalbeetle.fruitbat.atrio.ATRWriter;
 import com.metalbeetle.longan.stage.Chunker;
 import com.metalbeetle.longan.stage.LetterFinder;
@@ -41,9 +57,6 @@ public class LetterTestDataCategoriser implements KeyListener {
 			public void paint(Graphics g) {
 				g.setColor(Color.WHITE);
 				g.fillRect(0, 0, c.getWidth(), c.getHeight());
-				/*if (letterImg != null) {
-					g.drawImage(letterImg, c.getWidth() / 2 - letterImg.getWidth() / 2, c.getHeight() / 2 - letterImg.getHeight() / 2, c);
-				}*/
 				if (img == null || letterR == null) { return; }
 				g.drawImage(img, 0, 0, 200, 200,
 						letterR.x - 100,
--- src/com/metalbeetle/longan/Longan.java
+++ src/com/metalbeetle/longan/Longan.java
@@ -1,8 +1,23 @@
 package com.metalbeetle.longan;
 
+/*
+ * Copyright 2011 David Stark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.metalbeetle.longan.better.BetterChunker;
 import com.metalbeetle.longan.better.BetterLetterFinder;
-import com.metalbeetle.longan.better.EnglishDictPostProcessor;
 import com.metalbeetle.longan.better.HeuristicPostProcessor;
 import com.metalbeetle.longan.better.RotationFixingPreProcessor;
 import com.metalbeetle.longan.neuralnetwork.NNLI3PostProcessor;
@@ -28,7 +43,6 @@ public class Longan {
 		ArrayList<PostProcessor> pps = new ArrayList<PostProcessor>();
 		pps.add(new NNLI3PostProcessor());
 		pps.add(new HeuristicPostProcessor());
-		//pps.add(new EnglishDictPostProcessor());
 		return new Longan(
 			preps,
 			new BetterLetterFinder(),
