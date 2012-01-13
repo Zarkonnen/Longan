74c5e34ef03039a50af9663323e6dae463ec3cd3
David Stark <david.stark@zarkonnen.com>
2012-01-13 08:24:20 +0100

    Improvements on AggressiveLetterSplittingPostProcessor.

--- src/com/zarkonnen/longan/better/AggressiveLetterSplittingPostProcessor.java
+++ src/com/zarkonnen/longan/better/AggressiveLetterSplittingPostProcessor.java
@@ -38,27 +38,29 @@ public class AggressiveLetterSplittingPostProcessor implements PostProcessor {
 	static final int    MAX_RECURSION       = 3;
 	static final double OVERSIZE_THRESHOLD  = 1.5;
 	static final double OVERSIZE_ASPECT_THRESHOLD = 1.6;
+	static final double OVERSIZE_THRESHOLD_2  = 1.1;
+	static final double OVERSIZE_ASPECT_THRESHOLD_2 = 1.2;
 	static final double OVERSIZE_LOW_SCORE_BOUNDARY  = 0.93;
 	
 	int q = 0;
 	
-	ArrayList<Letter> split(Letter l, Letter source, Word word, Line line, Column c, Result result, Longan longan, double improvementThreshold, int recursion) {
+	ArrayList<Letter> split(Letter srcLetter, Letter topLetter, Word word, Line line, Column c, Result result, Longan longan, double improvementThreshold, int recursion) {
 		ArrayList<Letter> output = new ArrayList<Letter>();
-		ArrayList<Letter> lrs = sawApart(l, result.img);
+		ArrayList<Letter> lrs = sawApart(srcLetter, result.img);
 		if (lrs == null) { return null; }
 		for (Letter letter : lrs) {
 			if (letter.width == 0 || letter.height == 0) {
 				continue;
 			}
 			
-			longan.letterIdentifier.reIdentify(letter, source, word, line, c, result);
-			
+			longan.letterIdentifier.reIdentify(letter, topLetter, word, line, c, result);
+			String bestLetter = letter.bestLetter(); // qqDPS
 			double bestScore = letter.bestScore();
 			if (bestScore < improvementThreshold) {
 				if (recursion == MAX_RECURSION) {
 					return null;
 				} else {
-					ArrayList<Letter> sub = split(letter, source, word, line, c, result, longan,
+					ArrayList<Letter> sub = split(letter, topLetter, word, line, c, result, longan,
 							improvementThreshold, recursion + 1);
 					if (sub == null) {
 						return null;
@@ -68,10 +70,10 @@ public class AggressiveLetterSplittingPostProcessor implements PostProcessor {
 				}
 			} else {
 				boolean oversize = bestScore < OVERSIZE_LOW_SCORE_BOUNDARY &&
-						l.relativeSize > OVERSIZE_THRESHOLD &&
-						l.width / l.height > OVERSIZE_ASPECT_THRESHOLD;
+						letter.relativeSize > OVERSIZE_THRESHOLD_2 &&
+						letter.width / letter.height > OVERSIZE_ASPECT_THRESHOLD_2;
 				if (oversize) {
-					ArrayList<Letter> sub = split(letter, source, word, line, c, result, longan,
+					ArrayList<Letter> sub = split(letter, topLetter, word, line, c, result, longan,
 							bestScore, recursion + 1);
 					if (sub == null) {
 						output.add(letter);
@@ -93,11 +95,17 @@ public class AggressiveLetterSplittingPostProcessor implements PostProcessor {
 	public void process(Result result, Longan longan) {
 		for (Column c : result.columns) {
 			for (Line line : c.lines) {
+				
 				for (Word word : line.words) {
+					//System.out.println(q++ + word.toString());
+					if (q++ == 35) {
+						int x = 3;
+					}
 					lp: for (int i = 0; i < word.letters.size(); i++) {
 						Letter l = word.letters.get(i);
 						//System.out.print(l.bestLetter());
 						double bestScore = l.bestScore();
+						String bestLetter = l.bestLetter(); // qqDPS
 						//ArrayList<Letter> ls = new ArrayList<Letter>();
 						boolean oversize = bestScore < OVERSIZE_LOW_SCORE_BOUNDARY &&
 							l.relativeSize > OVERSIZE_THRESHOLD &&
